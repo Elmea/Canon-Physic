461860ba07edfa4294afd0ab4941516862e5f223
n masseteau <n.masseteau@student.isartdigital.com>
2023-01-18 15:17:20 +0100

    fix ui

--- src/UI.cpp
+++ src/UI.cpp
@@ -83,7 +83,7 @@ void UI::WorldParameters()
     if (ImGui::TreeNodeEx("World config",ImGuiTreeNodeFlags_DefaultOpen ))
     {
         SliderDouble("Gravity", &Data::WorldSetting::GRAVITY, -1, 50);
-        SliderDouble("Air Resistance", &Data::WorldSetting::airResistance, 0, 10);
+        SliderDouble("Air Resistance", &Data::WorldSetting::airResistance, 0.0001, 10);
         SliderDouble("Air Viscosity", &Data::WorldSetting::airViscosity, 0, 10);
         SliderDouble("Size Repere", &Data::WorldSetting::pixelPerMeter, 0.0001f, 10);
 
